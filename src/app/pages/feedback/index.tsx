3c1ae00b1af84b968d8f47ef371d5f8d382b6e32
MiguelCabeleira <1210632@isep.ipp.pt>
2022-04-25 18:22:40 +0100

    some chakra-ui changes

--- src/app/pages/feedback/index.tsx
+++ src/app/pages/feedback/index.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Center, Heading, Stack } from '@chakra-ui/react'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import { Textarea } from '@chakra-ui/react'
 import { Text } from '@chakra-ui/react'
+import { Divider } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
+import { SimpleGrid } from '@chakra-ui/react'
 
 
 function Feedback() {
@@ -11,14 +14,29 @@ function Feedback() {
         console.log(event.target.files[0]);
     }
     const [selectedUploadFile, setSelectedUploadFile] = useState(false);
+    const [selectedUploadFileCamera, setSelectedUploadFileCamera] = useState(false);
+    const [selectedUploadFileGallery, setSelectedUploadFileGallery] = useState(false);
+
+
     const [selectedUploadFeedbackTex, setSelectedUploadFeedbackText] = useState(false);
     const [selectedUploadAudio, setSelectedUploadAudio] = useState(false);
+    const [selectedUploadAudioMic, setSelectedUploadAudioMic] = useState(false);
+    const [selectedUploadAudioFiles, setSelectedUploadAudioFiles] = useState(false);
 
 
     const uploadFile = (event: any) => {
         setSelectedUploadFile(true);
         setSelectedUploadFeedbackText(false);
         setSelectedUploadAudio(false);
+    }
+
+    const uploadFileCamera = (event: any) => {
+        setSelectedUploadFileCamera(true);
+        setSelectedUploadFileGallery(false);
+    }
+    const uploadFileGallery = (event: any) => {
+        setSelectedUploadFileGallery(true);
+        setSelectedUploadFileCamera(false);
     }
 
     const uploadFeedbackText = (event: any) => {
@@ -31,6 +49,16 @@ function Feedback() {
         setSelectedUploadAudio(true);
         setSelectedUploadFile(false);
         setSelectedUploadFeedbackText(false);
+    }
+
+    const uploadAudioMic = (event: any) => {
+        setSelectedUploadAudioMic(true);
+        setSelectedUploadAudioFiles(false);
+    }
+
+    const uploadAudioFiles = (event: any) => {
+        setSelectedUploadAudioMic(false);
+        setSelectedUploadAudioFiles(true);
     }
 
     const cancelUpload = (event: any) => {
@@ -51,115 +79,66 @@ function Feedback() {
             <Heading as="h1" size="xl">
                 Feedback
             </Heading>
+            <p>Upload here your feedback.</p>
 
-            <Heading as="h6" size="xs">
-                Upload here your feedback
-            </Heading>
+            <SimpleGrid columns={[1, null, 3]} spacing='40px'>
+                <Box width='100%'>
+                    <Button colorScheme='blue' onClick={uploadFile}>Upload files</Button>
+                    <Button colorScheme='blue' onClick={uploadFeedbackText}>Upload feedback text</Button>
+                    <Button colorScheme='blue' onClick={uploadAudio}>Upload audio</Button>
+                    <br></br>
+                    <br></br>
+                    <Button colorScheme='blue'>Confirm</Button>
+                    <Button colorScheme='blue' onClick={cancelUpload}>Cancel</Button>
+                </Box>
+                <Box width='200%'>
+                    <div style={{ display: selectedUploadFile ? 'inline-block' : 'none' }}>
+                        <p>Upload images/videos</p>
 
+                        <Button colorScheme='blue' onClick={uploadFileCamera}>Use camera</Button>
+                        <Button colorScheme='blue' onClick={uploadFileGallery}>From gallery</Button>
 
-            <div style={{ display: selectedUploadFile ? 'inline-block' : 'none' }}>
-            <p>Upload images/videos</p>
+                        <div style={{ display: selectedUploadFileCamera ? 'inline-block' : 'none' }}>
 
-            <input type="file" accept="image/*,video/*" onChange={handleImageChange}></input>
-            <p>Upload images/videos ( use camera )</p>
-            <Button colorScheme='blue'>Confirm</Button>
-            <Button colorScheme='blue' onClick={cancelUpload}>Cancel</Button>
-            </div>
+                            <p>use camera</p>
+                        </div>
 
+                        <div style={{ display: selectedUploadFileGallery ? 'inline-block' : 'none' }}>
+                            <input type="file" accept="image/*,video/*" onChange={handleImageChange}></input>
+                        </div>
+                    </div>
 
-            <div style={{ display: selectedUploadFeedbackTex ? 'inline-block' : 'none' }}>
+                    <div style={{ display: selectedUploadFeedbackTex ? 'inline-block' : 'none' }}>
+                        <p>Upload text</p>
+                        <Textarea
+                            value={value}
+                            onChange={handleInputChange}
+                            placeholder='Place your text here...'
+                            size='sm'
+                        />
+                        <Text mb='8px'>Your text: {value}</Text>
+                    </div>
 
-            <p>Upload text</p>
-            <Textarea
-                value={value}
-                onChange={handleInputChange}
-                placeholder='Place your text here...'
-                size='sm'
-            />
-            <Text mb='8px'>Your text: {value}</Text>
+                    <div style={{ display: selectedUploadAudio ? 'inline-block' : 'none' }}>
 
-            <Button colorScheme='blue'>Confirm</Button>
-            <Button colorScheme='blue' onClick={cancelUpload}>Cancel</Button>
-            </div>
+                        <Button colorScheme='blue' onClick={uploadAudioMic}>Use mic</Button>
+                        <Button colorScheme='blue' onClick={uploadAudioFiles}>From files</Button>
 
-            <div style={{ display: selectedUploadAudio ? 'inline-block' : 'none' }}>
-            <p>Upload audio</p>
-            <input type="file" accept="audio/*" onChange={handleImageChange}></input>
-            <Button colorScheme='blue'>Confirm</Button>
-            <Button colorScheme='blue' onClick={cancelUpload}>Cancel</Button>
+                        <div style={{ display: selectedUploadAudioFiles ? 'inline-block' : 'none' }}>
+                            <p>Upload audio</p>
+                            <input type="file" accept="audio/*" onChange={handleImageChange}></input>
+                        </div>
 
-            <p>Upload audio ( use mic )</p>
-            </div>
-
-            <br></br>
-            <br></br>
-            <br></br>
-
-
-            <Button colorScheme='blue' onClick={uploadFile}>Upload files</Button>
-            <Button colorScheme='blue'onClick={uploadFeedbackText}>Upload feedback text</Button>
-            <Button colorScheme='blue'onClick={uploadAudio}>Upload audio</Button>
-
-
-
+                        <div style={{ display: selectedUploadAudioMic ? 'inline-block' : 'none' }}>
+                            <p>Upload audio ( use mic )</p>
+                        </div>
+                    </div>
+                </Box>
+            </SimpleGrid>
         </>
-
-
     )
 }
 
 
-
-/**
-const handleImageChange: any = (event: any) => {
-    console.log(event.target.files[0]);
-}
- */
 export default Feedback
 
-
-/**
-    render() {
-        return (
-            <div>
-                <Heading as="h1" size="xl">
-                    Feedback
-                </Heading>
-
-                <Heading as="h6" size="xs">
-                    Upload here your feedback
-                </Heading>
-
-
-                 
-
-                <div style={{ display: selectedUploadFile ? 'inline-block' : 'none' }}>
-                <p>Upload images/videos</p>
-
-                <input type="file" accept="image/*,video/*" onChange={handleImageChange}></input>
-                <Button colorScheme='blue'>Confirm</Button>
-                <p>Upload images/videos ( use camera )</p>
-
-                </div>
-
-                <p>Upload text</p>
-                <Textarea placeholder='Here is a sample placeholder' />
-
-                <p>Upload audio</p>
-                <input type="file" accept="audio/*" onChange={handleImageChange}></input>
-                <Button colorScheme='blue'>Confirm</Button>
-
-
-                <p>Upload audio ( use mic )</p>
-
-                <Button colorScheme='blue'>Upload files</Button>
-                <Button colorScheme='blue'>Upload feedback text</Button>
-                <Button colorScheme='blue'>Upload audio</Button>
-
-
-            </div>
-        );
-    }
-
-}
- */
