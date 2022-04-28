11f8b77ae11076ee00b74cf21487a92b87d49f25
Cárina Alas <carina.alas@farfetch.com>
2022-04-28 21:29:51 +0100

    playing around

--- src/app/pages/admin/index.tsx
+++ src/app/pages/admin/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Select } from '@chakra-ui/react'
 
 function Admin() {
 
     return (
         <>
         Admin
+        <Select placeholder='Select option'>
+            <option value='option1'>Option 1</option>
+            <option value='option2'>Option 2</option>
+            <option value='option3'>Option 3</option>
+        </Select>
         </>
     )
 }
