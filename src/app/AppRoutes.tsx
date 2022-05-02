b8ffbe9a9ad6722278e3f0c745c8a86a5da858f2
Cárina Alas <carina.alas@farfetch.com>
2022-05-02 20:42:11 +0100

    Added AdminFeedback page

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/home';
 import Login from './pages/login';
-import Admin from './pages/admin';
+import {AdminFeedback} from './pages/admin';
 import Settings from './pages/settings';
 import Feedback from './pages/feedback';
 
@@ -11,7 +11,7 @@ function AppRoutes() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/admin" element={<Admin />} />
+      <Route path="/admin" element={<AdminFeedback />} />
       <Route path="/settings" element={<Settings />} />
       <Route path="/feedback" element={<Feedback />} />
     </Routes>
--- src/app/pages/admin/index.tsx
+++ src/app/pages/admin/index.tsx
@@ -1,18 +1,75 @@
-import React from 'react';
-import { Select } from '@chakra-ui/react'
+import React, { ReactNode } from 'react';
+import { Box,
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Text } from '@chakra-ui/react'
+    import ReactAudioPlayer from 'react-audio-player';
 
-function Admin() {
+    interface Feedback {
+        date: string,
+        feedback: string,
+        name: string,
+        type: string, // could be an enum
+      }
+      
+      const array1: Feedback[] = [
+        {date: "qui. 31/03 19:32", feedback: "Não tenho nada a dizer a aplicação é incrivel", name: "João das Neves", type: "text"},
+        {date: "qui. 31/03 20:02", feedback: "Os devs são muito fofinhos", name: "John", type: "text"},
+        {date: "qui. 31/03 23:20", feedback: "https://www.w3schools.com/images/w3schools_green.jpg", name: "Wanda Maximoff", type: "image"},
+        {date: "qui. 31/03 23:45", feedback: "my_audio_file.ogg", name: "You don't wanna know", type: "audio"},
+      ];
 
-    return (
-        <>
-        Admin
-        <Select placeholder='Select option'>
-            <option value='option1'>Option 1</option>
-            <option value='option2'>Option 2</option>
-            <option value='option3'>Option 3</option>
-        </Select>
-        </>
-    )
-}
 
-export default Admin
+      export class AdminFeedback extends React.Component<{},{}>{
+
+        private getFeedback(feedback: string, type:string):ReactNode  {
+            if(type === "image"){
+                return <div>
+                    <img src={feedback} alt=""></img>
+                </div>
+            }
+            else if(type==="audio"){
+                return <ReactAudioPlayer
+                src= {feedback}
+                autoPlay
+                controls
+              />
+            }
+            else{
+                return <Text fontSize='md'>
+                {feedback}
+            </Text>
+            }
+        }
+
+        render(){
+            return(
+                <>
+                Admin Feedback Page
+                {array1.map(({ date, feedback, name, type }) => (
+                <Accordion allowToggle>
+                    <AccordionItem>
+                        <h2>
+                        <AccordionButton bg='isepBrick.300' w='100%' p={4} color='isepGrey.500'>
+                            <Box flex='1' textAlign='left' textColor='#000000'>
+                                {date}
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4}>
+                            {this.getFeedback(feedback, type)}
+                            <Text fontSize='xs' as='i'>
+                                {name}
+                            </Text>
+                        </AccordionPanel>
+                    </AccordionItem>
+                </Accordion>
+                ))}
+                </>
+            )
+        }
+      }
