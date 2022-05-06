9e5d11957c71a60c4c621c1c6549092b4cd474ef
MiguelCabeleira <1210632@isep.ipp.pt>
2022-05-06 12:08:43 +0100

    some changes to UI

--- src/app/pages/feedback/index.tsx
+++ src/app/pages/feedback/index.tsx
@@ -177,8 +177,8 @@ function Feedback() {
                     <Box>
                         <Center>
                             <ButtonGroup marginTop='20%' marginBottom='10%'>
-                                <Button width='67px' height='67px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadFile}><img src="https://s3-alpha-sig.figma.com/img/5c37/7770/eced2bac8d7213431dc4807a6e524329?Expires=1652659200&Signature=UfMtDJqYCdsp1JugqHNKOyyH35cL7EbQ05e8uwmf-k4K3ShQwqXn676qqLZs7ma0xQUg7RZiz2-vu04xKFca8btN8AhHxl5U83dd3zYjyOtOl2rIzQKnBmfPFtCodlUlyBUwHb2ul7Qniji08dJts7Sh81p7JSWWtP3CeZMiFl9kg~Wb8nXhWhYg9dJyaUZUd0YuAhSwt8~DYH02fQd9JUbaB84neQplwy0LT9zIwTBHaBR5dyawTeM03J5MG3y-uAz1-p1bhpaByil2I35RlfgjpoC72HttNyajOMb3amfvr2AIUqrnu9zjkaraUHkHtGrzoZMMc40PMBskJoMjIw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" /></Button>
-                                <Button width='67px' height='67px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadFeedbackText}><img src='https://s3-alpha-sig.figma.com/img/2f03/7f76/ea4b824d46259756ee696c6311a3e9c2?Expires=1652659200&Signature=M78iAFpzCO4GFcvhqolOfU9obPGhP2mtJZxVIwPgAleeHuQz1Ka7pCT92McBPr-yxkhFupd~RjHYjtStFlsQotJyLD9WWVDJUFBxekzDDV9YTl70vsbtB8YdOxI3xGs-Ui0cKqCbtNECDtvgHd6AoPGSmULb-WsWE669lnH8ZCba7CSRqh3qYCHCMxaf6qcAPhg07jDXGKVWzkTkKCW6qYRxG69060oFzu-R4a3u9K4SQvqa2AEU1NlWT2Mr4d7ImbagetBQBQSTQdoYhX4gatI7EZMqix3qt5UZkX3048OgYsU0OO0oMceGqdiHjWAYkvnF~ztPUTzdbX7xQp52lQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img></Button>
+                                <Button width='67px' height='67px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadFeedbackText}><img src="https://s3-alpha-sig.figma.com/img/5c37/7770/eced2bac8d7213431dc4807a6e524329?Expires=1652659200&Signature=UfMtDJqYCdsp1JugqHNKOyyH35cL7EbQ05e8uwmf-k4K3ShQwqXn676qqLZs7ma0xQUg7RZiz2-vu04xKFca8btN8AhHxl5U83dd3zYjyOtOl2rIzQKnBmfPFtCodlUlyBUwHb2ul7Qniji08dJts7Sh81p7JSWWtP3CeZMiFl9kg~Wb8nXhWhYg9dJyaUZUd0YuAhSwt8~DYH02fQd9JUbaB84neQplwy0LT9zIwTBHaBR5dyawTeM03J5MG3y-uAz1-p1bhpaByil2I35RlfgjpoC72HttNyajOMb3amfvr2AIUqrnu9zjkaraUHkHtGrzoZMMc40PMBskJoMjIw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" /></Button>
+                                <Button width='67px' height='67px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadFile}><img src='https://s3-alpha-sig.figma.com/img/2f03/7f76/ea4b824d46259756ee696c6311a3e9c2?Expires=1652659200&Signature=M78iAFpzCO4GFcvhqolOfU9obPGhP2mtJZxVIwPgAleeHuQz1Ka7pCT92McBPr-yxkhFupd~RjHYjtStFlsQotJyLD9WWVDJUFBxekzDDV9YTl70vsbtB8YdOxI3xGs-Ui0cKqCbtNECDtvgHd6AoPGSmULb-WsWE669lnH8ZCba7CSRqh3qYCHCMxaf6qcAPhg07jDXGKVWzkTkKCW6qYRxG69060oFzu-R4a3u9K4SQvqa2AEU1NlWT2Mr4d7ImbagetBQBQSTQdoYhX4gatI7EZMqix3qt5UZkX3048OgYsU0OO0oMceGqdiHjWAYkvnF~ztPUTzdbX7xQp52lQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img></Button>
                                 <Button width='67px' height='67px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadAudio}><img src='https://s3-alpha-sig.figma.com/img/9570/6275/02fe629724c6451bfc58e8b408959b7f?Expires=1652659200&Signature=ersh~djo0M0azAQXkkTt4atepCPDgD7pfkNhGnpDDINizApqLzAFk8SMm-PpkHFaMqzOW-xSqAfeDd8V8mXslKQY7g97M6zXewSuftfpQkewSOmHfT8s3OINRHV3iqDEW4z8f0j~8q5D9Q7iEhCOVXWWiklU9PYEpJOrXNpTib75MigEcus2~vkeBGPNvtWLUxXlbKELRs1lJ4Bi~qr2hxGiRdeGELKk~WBl0qchXBivzZEevHwInxe7Huusa1Ug1QE3CFctUOk2xxDU569K4PKG7siFbZQ4gNZUlUXsZa9w6bFmzwBZHuu7c8gph3ooZQXf3c65Ew99zAjkdd5CxA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img></Button>
                             </ButtonGroup>
                         </Center>
@@ -299,11 +299,14 @@ function Feedback() {
                     </Box>
 
                     <Box>
-                        <Center>
-                            <Button backgroundColor='#A2543D' borderRadius='200px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' height='47px' width='206px' marginBottom='2%'>Confirm</Button>
+                    <Center>
+                            <Button backgroundColor='#A2543D' borderRadius='200px' _hover={{ bg: '#CE7E5C' }} color='#FFFFFF' borderColor='#CE7E5C' height='47px' width='206px' marginBottom='2%'>CONFIRM</Button>
                         </Center>
                         <Center>
-                            <Button _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='200px' variant='outline' height='47px' width='206px' onClick={cancelUpload}>Cancel</Button>
+                            <Button _hover={{ bg: '#DA8E71' }} borderColor='#A2543D' borderRadius='200px' color='#A2543D' variant='outline' height='47px' width='206px' marginBottom='2%' onClick={cancelUpload}>CANCEL</Button>
+                        </Center>
+                        <Center>
+                            <Button _hover={{ bg: '#A2543D' }} borderColor='#DA8E71' borderRadius='200px' color='#DA8E71' variant='outline' height='47px' width='206px'>SENT FEEDBACK</Button>
                         </Center>
                     </Box>
 
@@ -312,76 +315,43 @@ function Feedback() {
 
 
             <BrowserView>
-            
-            <SimpleGrid columns={[2]}>
+
+                <SimpleGrid columns={[2]}>
                     <Box>
                         <Center>
-                            <ButtonGroup marginTop='20%' marginBottom='10%'>
-                                <Button width='67px' height='67px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadFile}><img src="https://s3-alpha-sig.figma.com/img/5c37/7770/eced2bac8d7213431dc4807a6e524329?Expires=1652659200&Signature=UfMtDJqYCdsp1JugqHNKOyyH35cL7EbQ05e8uwmf-k4K3ShQwqXn676qqLZs7ma0xQUg7RZiz2-vu04xKFca8btN8AhHxl5U83dd3zYjyOtOl2rIzQKnBmfPFtCodlUlyBUwHb2ul7Qniji08dJts7Sh81p7JSWWtP3CeZMiFl9kg~Wb8nXhWhYg9dJyaUZUd0YuAhSwt8~DYH02fQd9JUbaB84neQplwy0LT9zIwTBHaBR5dyawTeM03J5MG3y-uAz1-p1bhpaByil2I35RlfgjpoC72HttNyajOMb3amfvr2AIUqrnu9zjkaraUHkHtGrzoZMMc40PMBskJoMjIw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" /></Button>
-                                <Button width='67px' height='67px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadFeedbackText}><img src='https://s3-alpha-sig.figma.com/img/2f03/7f76/ea4b824d46259756ee696c6311a3e9c2?Expires=1652659200&Signature=M78iAFpzCO4GFcvhqolOfU9obPGhP2mtJZxVIwPgAleeHuQz1Ka7pCT92McBPr-yxkhFupd~RjHYjtStFlsQotJyLD9WWVDJUFBxekzDDV9YTl70vsbtB8YdOxI3xGs-Ui0cKqCbtNECDtvgHd6AoPGSmULb-WsWE669lnH8ZCba7CSRqh3qYCHCMxaf6qcAPhg07jDXGKVWzkTkKCW6qYRxG69060oFzu-R4a3u9K4SQvqa2AEU1NlWT2Mr4d7ImbagetBQBQSTQdoYhX4gatI7EZMqix3qt5UZkX3048OgYsU0OO0oMceGqdiHjWAYkvnF~ztPUTzdbX7xQp52lQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img></Button>
-                                <Button width='67px' height='67px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadAudio}><img src='https://s3-alpha-sig.figma.com/img/9570/6275/02fe629724c6451bfc58e8b408959b7f?Expires=1652659200&Signature=ersh~djo0M0azAQXkkTt4atepCPDgD7pfkNhGnpDDINizApqLzAFk8SMm-PpkHFaMqzOW-xSqAfeDd8V8mXslKQY7g97M6zXewSuftfpQkewSOmHfT8s3OINRHV3iqDEW4z8f0j~8q5D9Q7iEhCOVXWWiklU9PYEpJOrXNpTib75MigEcus2~vkeBGPNvtWLUxXlbKELRs1lJ4Bi~qr2hxGiRdeGELKk~WBl0qchXBivzZEevHwInxe7Huusa1Ug1QE3CFctUOk2xxDU569K4PKG7siFbZQ4gNZUlUXsZa9w6bFmzwBZHuu7c8gph3ooZQXf3c65Ew99zAjkdd5CxA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img></Button>
+                            <ButtonGroup marginTop='20%' marginBottom='10%' spacing='12'>
+                                <Button width='93px' height='93px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadFeedbackText}><img src="https://s3-alpha-sig.figma.com/img/5c37/7770/eced2bac8d7213431dc4807a6e524329?Expires=1652659200&Signature=UfMtDJqYCdsp1JugqHNKOyyH35cL7EbQ05e8uwmf-k4K3ShQwqXn676qqLZs7ma0xQUg7RZiz2-vu04xKFca8btN8AhHxl5U83dd3zYjyOtOl2rIzQKnBmfPFtCodlUlyBUwHb2ul7Qniji08dJts7Sh81p7JSWWtP3CeZMiFl9kg~Wb8nXhWhYg9dJyaUZUd0YuAhSwt8~DYH02fQd9JUbaB84neQplwy0LT9zIwTBHaBR5dyawTeM03J5MG3y-uAz1-p1bhpaByil2I35RlfgjpoC72HttNyajOMb3amfvr2AIUqrnu9zjkaraUHkHtGrzoZMMc40PMBskJoMjIw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" /></Button>
+                                <Button width='93px' height='93px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadFile}><img src='https://s3-alpha-sig.figma.com/img/2f03/7f76/ea4b824d46259756ee696c6311a3e9c2?Expires=1652659200&Signature=M78iAFpzCO4GFcvhqolOfU9obPGhP2mtJZxVIwPgAleeHuQz1Ka7pCT92McBPr-yxkhFupd~RjHYjtStFlsQotJyLD9WWVDJUFBxekzDDV9YTl70vsbtB8YdOxI3xGs-Ui0cKqCbtNECDtvgHd6AoPGSmULb-WsWE669lnH8ZCba7CSRqh3qYCHCMxaf6qcAPhg07jDXGKVWzkTkKCW6qYRxG69060oFzu-R4a3u9K4SQvqa2AEU1NlWT2Mr4d7ImbagetBQBQSTQdoYhX4gatI7EZMqix3qt5UZkX3048OgYsU0OO0oMceGqdiHjWAYkvnF~ztPUTzdbX7xQp52lQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img></Button>
+                                <Button width='93px' height='93px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='62px' variant='outline' onClick={uploadAudio}><img src='https://s3-alpha-sig.figma.com/img/9570/6275/02fe629724c6451bfc58e8b408959b7f?Expires=1652659200&Signature=ersh~djo0M0azAQXkkTt4atepCPDgD7pfkNhGnpDDINizApqLzAFk8SMm-PpkHFaMqzOW-xSqAfeDd8V8mXslKQY7g97M6zXewSuftfpQkewSOmHfT8s3OINRHV3iqDEW4z8f0j~8q5D9Q7iEhCOVXWWiklU9PYEpJOrXNpTib75MigEcus2~vkeBGPNvtWLUxXlbKELRs1lJ4Bi~qr2hxGiRdeGELKk~WBl0qchXBivzZEevHwInxe7Huusa1Ug1QE3CFctUOk2xxDU569K4PKG7siFbZQ4gNZUlUXsZa9w6bFmzwBZHuu7c8gph3ooZQXf3c65Ew99zAjkdd5CxA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'></img></Button>
                             </ButtonGroup>
                         </Center>
 
+                        <Center>
+                            <Button backgroundColor='#A2543D' borderRadius='200px' _hover={{ bg: '#CE7E5C' }} color='#FFFFFF' borderColor='#CE7E5C' height='47px' width='529px' marginBottom='2%'>CONFIRM</Button>
+                        </Center>
+                        <Center>
+                            <Button _hover={{ bg: '#DA8E71' }} borderColor='#A2543D' borderRadius='200px' color='#A2543D' variant='outline' height='47px' width='529px' marginBottom='2%' onClick={cancelUpload}>CANCEL</Button>
+                        </Center>
+                        <Center>
+                            <Button _hover={{ bg: '#A2543D' }} borderColor='#DA8E71' borderRadius='200px' color='#DA8E71' variant='outline' height='47px' width='529px'>SENT FEEDBACK</Button>
+                        </Center>
                     </Box>
 
                     <Box>
 
                         <div style={{ display: selectedUploadFile ? 'block' : 'none' }}>
-                            <Center>
-                                <p>Upload images/videos</p>
-                            </Center>
-                            <Center>
-                                <Button backgroundColor='#CE7E5C' color='#FFFFFF' borderRadius='200px' _hover={{ bg: '#A2543D' }} borderColor='#A2543D' width='206px' height='47px' marginBottom='2%' onClick={uploadFileCamera}>Use camera</Button>
-                            </Center>
-                            <Center>
-                                <Button backgroundColor='#CE7E5C' color='#FFFFFF' borderRadius='200px' _hover={{ bg: '#A2543D' }} borderColor='#A2543D' width='206px' height='47px' onClick={uploadFileGallery}>From gallery</Button>
-                            </Center>
-                            <div style={{ display: selectedUploadFileCamera ? 'block' : 'none' }}>
-
-                                <div className='camera'>
-                                    <Center>
-                                        <Webcam ref={webRef}></Webcam>
-                                    </Center>
-                                    <Center>
-                                        <Button colorScheme='blue' onClick={() => {
-                                            showImage();
-                                        }}>Take a photo</Button>
-                                    </Center>
-                                    <Center>
-                                        <ReactMediaRecorder
-                                            video
-                                            render={({ status, startRecording, stopRecording, mediaBlobUrl }) => (
-                                                <div>
-                                                    <p>status: {status}</p>
-                                                    <Button colorScheme='blue' onClick={startRecording}>Start Recording</Button>
-                                                    <Button colorScheme='blue' onClick={stopRecording}>Stop Recording</Button>
-
-                                                    <video src={mediaBlobUrl || undefined} controls autoPlay loop></video>
-
-                                                </div>
-                                            )}
-                                        />
-                                    </Center>
-                                </div>
-
-                                <div className={'result' + (hasPhoto ? 'hasPhoto' : '')} >
-                                    <Center>
-                                        <img src={img || undefined}></img>
-                                    </Center>
-                                    <Center>
-                                        <Button colorScheme='blue' onClick={closePhoto}>close</Button>
-                                    </Center>
-                                </div>
-                            </div>
-
-                            <div style={{ display: selectedUploadFileGallery ? 'block' : 'none' }}>
-                                <Center>
-                                    <input type="file" accept="image/*,video/*" onChange={handleImageChange}></input>
-                                </Center>
-                            </div>
+                            <Box backgroundColor='#FCE5D7' p={4} color='#575757'> 
+                                    <label onChange={handleImageChange} htmlFor="formId">
+                                        <input name="" type="file" accept="image/*,video/*" id="formId" hidden />
+                                        <Center>
+                                        <img width='50%' src="https://s3-alpha-sig.figma.com/img/e61d/d2cb/0a63e30674435607b06b4d6b466384f5?Expires=1652659200&Signature=URLDx3p2s8~-sw~0ToQSzVYZQntaSlYpFwCKnw-Wb94HVTr448gEWOEPMkrMPNb0tnTsMiiCKTusgmmPnk5EJr3lK66zGOMbgUxqPvJdx6i0Tv6umys4UTeMwq~MAdwiPNTce9HQ5rBRWOWgVE3EmDgiAhh-dfM6U73VfeUShGkeU5yNv-yifAPBU5r~mgx2dh3YoMIQ4iKfS--pzUFW4BE6YMdNiPoDfMwMpJrYdaFPQpmGK0lsJPdEn5nxnzVLQkn-JrH9uyGi0FXsL9cxCH6Ofcy5BDEKtL90dk3jnEzc3YXvlXKJaQ2M8Z40yW7HaF5fmp987hV7SpJZjSsipw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" />
+                                        </Center>
+                                        <Center>
+                                        <h1>Click or drag photo/video</h1>
+                                        </Center>
+                                    </label>
+                            </Box>
                         </div>
-
 
                         <div style={{ display: selectedUploadFeedbackTex ? 'inline' : 'none' }}>
                             <Center><p>Upload text</p></Center>
@@ -437,20 +407,10 @@ function Feedback() {
 
                         </div>
                     </Box>
-
-                    <Box>
-                        <Center>
-                            <Button backgroundColor='#A2543D' borderRadius='200px' _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' height='47px' width='206px' marginBottom='2%'>Confirm</Button>
-                        </Center>
-                        <Center>
-                            <Button _hover={{ bg: '#CE7E5C' }} borderColor='#CE7E5C' borderRadius='200px' variant='outline' height='47px' width='206px' onClick={cancelUpload}>Cancel</Button>
-                        </Center>
-                    </Box>
-
                 </SimpleGrid>
 
             </BrowserView>
-            
+
 
 
         </>
