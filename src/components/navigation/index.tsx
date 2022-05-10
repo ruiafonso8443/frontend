345b729db71472839d46a984f4731fdb8bfd09fb
renguitto <duarte.marques@inygon.com>
2022-05-10 19:19:25 +0100

    created mobile navbar

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import "./index.scss";
-
 import { ReactComponent as Logo } from "../../assets/svg/logo.svg";
 import { ReactComponent as SingOut } from "../../assets/svg/signOut.svg";
-
 import useTranslation from "../../i18n/use-translation";
 import i18n from "../../i18n";
 import { useStoreDispatch, useStoreSelector } from "../../store";
@@ -15,8 +13,12 @@ import {
   goToLoginPage,
 } from "../../store/navigation-reducer";
 import Page from "../../types/pages";
-
+import { BrowserView, MobileView } from 'react-device-detect';
 import { MouseEvent } from "react";
+import { Box, Center, Flex, Icon, IconButton } from "@chakra-ui/react";
+import { BsFillChatFill, BsGearFill } from 'react-icons/bs'
+import { AiFillHome } from 'react-icons/ai'
+
 
 function Navigation() {
   const { t } = useTranslation();
@@ -41,81 +43,138 @@ function Navigation() {
     if (id === "settings") dispatch(goSettingsPage());
   };
 
+  const handleNavigationClickMobile = (id: string) => {
+    if (id === "homepage") dispatch(goToHomePage());
+    if (id === "feedback") dispatch(goToFeedBackPage());
+    if (id === "settings") dispatch(goSettingsPage());
+  }
+
   const handleLogoutClick = () => {
     // TODO the Bloody Logout when user is ready
 
     dispatch(goToLoginPage());
   };
 
+  const renderMobileNavItem = (pageId: number, icon: any, url: any) => {
+    return (
+      <>
+
+        <IconButton
+          aria-label={`navto${url}`}
+          variant='ghost'
+          rounded='100'
+          _focus={{
+            boxShadow: 'none'
+          }}
+          _hover={{
+            backgroundColor: 'isepBrick.400'
+          }}
+          color={pageId === page ? 'isepBrick.300' : 'isepBrick.400'}
+          icon={<Icon
+            as={icon}
+            w={6}
+            h={6}
+          />}
+          onClick={() => handleNavigationClickMobile(url)}
+        />
+
+      </>
+    )
+  }
+
   return (
-    <div className="container">
-      {page !== Page.Login && (
-        <div className="page_nav">
-          <Logo className="app_logo" />
-          <span
-            id="homepage"
-            onClick={handleNavigationClick}
-            className="first_child"
-            style={{
-              fontWeight: page === 1 ? "700" : "400",
-            }}
+    <>
+      <MobileView>
+        <Center>
+          <Box
+            w="234px"
+            h="52px"
+            bg='isepBrick.500'
+            rounded='100'
+            position='fixed'
+            bottom='30'
           >
-            {t("homepage")}
-          </span>
-          <span
-            id="feedback"
-            onClick={handleNavigationClick}
-            style={{
-              fontWeight: page === 2 ? "700" : "400",
-            }}
-          >
-            {t("feedback")}
-          </span>
-          <span
-            id="settings"
-            onClick={handleNavigationClick}
-            style={{
-              fontWeight: page === 3 ? "700" : "400",
-            }}
-          >
-            {t("settings")}
-          </span>
+            <Flex direction='row' justifyContent='space-around' py='7px' px='14px'>
+              {renderMobileNavItem(2, BsFillChatFill, 'feedback')}
+              {renderMobileNavItem(1, AiFillHome, 'homepage')}
+              {renderMobileNavItem(3, BsGearFill, 'settings')}
+            </Flex>
+          </Box>
+        </Center>
+      </MobileView>
+
+      <BrowserView>
+        <div className="container">
+          {page !== Page.Login && (
+            <div className="page_nav">
+              <Logo className="app_logo" />
+              <span
+                id="homepage"
+                onClick={handleNavigationClick}
+                className="first_child"
+                style={{
+                  fontWeight: page === 1 ? "700" : "400",
+                }}
+              >
+                {t("homepage")}
+              </span>
+              <span
+                id="feedback"
+                onClick={handleNavigationClick}
+                style={{
+                  fontWeight: page === 2 ? "700" : "400",
+                }}
+              >
+                {t("feedback")}
+              </span>
+              <span
+                id="settings"
+                onClick={handleNavigationClick}
+                style={{
+                  fontWeight: page === 3 ? "700" : "400",
+                }}
+              >
+                {t("settings")}
+              </span>
+            </div>
+          )}
+
+          <div className="lang_options">
+            {page === Page.Login && (
+              <span className="about_us_copy">{t("about_us")}</span>
+            )}
+
+            <div className="lang_options_wrapper">
+              <span
+                onClick={() => {
+                  setLangCode("PT");
+                }}
+                style={{
+                  fontWeight: langCod === "PT" ? "700" : "300",
+                }}
+              >
+                PT
+              </span>
+              <span
+                onClick={() => {
+                  setLangCode("EN");
+                }}
+                style={{
+                  fontWeight: langCod === "EN" ? "700" : "300",
+                }}
+              >
+                EN
+              </span>
+            </div>
+
+            {page !== Page.Login && (
+              <SingOut className="logout_icon" onClick={handleLogoutClick} />
+            )}
+          </div>
         </div>
-      )}
+      </BrowserView>
 
-      <div className="lang_options">
-        {page === Page.Login && (
-          <span className="about_us_copy">{t("about_us")}</span>
-        )}
-
-        <div className="lang_options_wrapper">
-          <span
-            onClick={() => {
-              setLangCode("PT");
-            }}
-            style={{
-              fontWeight: langCod === "PT" ? "700" : "300",
-            }}
-          >
-            PT
-          </span>
-          <span
-            onClick={() => {
-              setLangCode("EN");
-            }}
-            style={{
-              fontWeight: langCod === "EN" ? "700" : "300",
-            }}
-          >
-            EN
-          </span>
-        </div>
-
-        {page !== Page.Login && (
-          <SingOut className="logout_icon" onClick={handleLogoutClick} />
-        )}
-      </div>
-    </div>
+    </>
   );
 }
 
